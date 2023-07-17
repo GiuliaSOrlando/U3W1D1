81ccdbcafe75d5fcf1d6da12c4120136ff640e31
GiuliaSOrlando <134381769+GiuliaSOrlando@users.noreply.github.com>
2023-07-17 16:47:08 +0200

    Consegna

---
+++ tsc/script.js
@@ -0,0 +1,39 @@
+"use strict";
+const submitBtn = document.getElementById("submit-button");
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    // Genero un numero casuale
+    let randomNum = 100;
+    console.log(randomNum);
+    // Memorizzo in una variabile l'input del primo giocatore
+    let inputFirstPlayer = parseFloat(document.getElementById("first-player").value);
+    // Memorizzo in un'altra variabile l'input del secondo giocatore
+    let inputSecondPlayer = parseFloat(document.getElementById("second-player").value);
+    // Assegno ad una variabile il numero di tentativi disponibili per giocatore
+    if (inputFirstPlayer === randomNum && inputSecondPlayer === randomNum) {
+        console.log("Entrambi i giocatori hanno indovinato il numero");
+        alert(`Il numero estratto è ${randomNum},  perciò entrambi i giocatori hanno indovinato il numero`);
+    }
+    else if (inputFirstPlayer !== randomNum &&
+        inputSecondPlayer === randomNum) {
+        console.log("Ha indovinato solo il secondo giocatore");
+        alert(`Il numero estratto è ${randomNum},  perciò ha indovinato solo il secondo giocatore`);
+    }
+    else if (inputFirstPlayer === randomNum &&
+        inputSecondPlayer !== randomNum) {
+        console.log("Ha indovinato solo il primo giocatore");
+        alert(`Il numero estratto è ${randomNum},  perciò ha indovinato solo il primo giocatore`);
+    }
+    else if (inputFirstPlayer !== randomNum &&
+        inputSecondPlayer !== randomNum) {
+        if (Math.abs(randomNum - inputFirstPlayer) >
+            Math.abs(randomNum - inputSecondPlayer)) {
+            console.log("Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più");
+            alert(`Il numero estratto è ${randomNum},  perciò nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più`);
+        }
+        else {
+            console.log("Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più");
+            alert(`Il numero estratto è ${randomNum},  perciò nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più`);
+        }
+    }
+});
